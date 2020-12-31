2232130088f35e53a335b56bd0c1fc2ee9fdd77f
Roland Oben <neborol@gmail.com>
2020-12-31 08:02:17 -0500

    Added some dummy redux registration code, just to show that redux is properly setup.

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Register = () => {
+import { register } from '../../redux/actions/auth';
+
+
+const Register = ({ dev, isAuthenticated, register }) => {
+
+    useEffect(() => {
+        // Hard-coding the supposed login values for now.
+       const oDev = {
+            name: 'Roland Oben',
+            email: 'myname@example.com',
+            password: 'abc1234'
+        };
+
+        register(oDev); // Use the REGISTER_SUCCES action creator to dispatch Register success.       
+    }, []);
+
     return (
-        <div>
-            <h1>Register and create your dev-profile</h1>
-        </div>
+        // Get the state slice "auth.user" passed by the mapStateToProps method as props.dev, and read the values.
+        dev && (
+            isAuthenticated && (
+            <div>
+                <h1>Is the current User authenticated? { isAuthenticated ? ': True' : ': False.' }</h1>
+                <ul>
+                    <li>Name: { dev.name }</li>
+                    <li>Name: { dev.email }</li>
+                </ul>
+            </div>
+            )            
+        )
     )
 }
 
-export default Register
+Register.propTypes = {
+    register: PropTypes.func.isRequired,
+    dev: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    dev: state.auth.user
+});
+
+// Map state to the props of this Register component while destructuring at the same time, and these props could be retrieved by passing destructured props in the component parameters input.
+export default connect(mapStateToProps, { register })(Register);
