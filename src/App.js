7e7fbac3ab367ae5455c4b0c1eadb12e0ab2e065
Roland Oben <neborol@gmail.com>
2020-12-31 03:31:29 -0500

    Built all the initial routes and the different app navigation pages required. Routes setup in App.js and working.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import MyProfile from './components/profile/MyProfile';
+import ProjectDashboard from './components/dashboard/ProjectDashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import Approved from './components/approved/Approved';
+import Pool from './components/pool/Pool';
+import './App.scss';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Plain React new slate.</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <hr />
+      <Route exact path='/' component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/profile' component={MyProfile} />
+            <Route exact path='/project-dashboard' component={ProjectDashboard} /> 
+            <Route exact path='/create-profile' component={CreateProfile} />
+            <Route exact path='/edit-profile' component={EditProfile} /> 
+            <Route exact path='/approved' component={Approved} /> 
+            <Route exact path='/ideas-pool' component={Pool} /> 
+          </Switch>
+        </section>
+        
+    </Router>
   );
 }
 
