02ec631c1c772612ceb1da0fa5d2a979c75caa34
Roland Oben <neborol@gmail.com>
2021-01-01 02:57:35 -0500

    Added the full login forms and functionality, pending form-validation, in order to register and also login, which works with the Navigation links.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../redux/actions/auth';
 
-const Login = () => {
-    return (
-        <div>
-            <h1>Login to your account</h1>
-        </div>
-    )
+
+
+const Login = ({login, isAuthenticated}) => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    // Destructure the login properties from formData in order to avoid prefixing formData to the properties
+    const {  email, password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+        login(email, password);
+    }
+
+    // Redirect if logged in
+    if (isAuthenticated) {
+        return <Redirect to="/project-dashboard" />
+    }
+
+    return <>
+            <h1 className="large text-primary">Sign In</h1>
+            {/* <Spinner loading={false} /> */}
+            <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+
+                <div className="form-group">
+                <input 
+                    type="email"
+                    placeholder="Email Address"
+                    name="email"
+                    value={email}
+                    onChange={ e => onChange(e) }
+                    required
+                />
+                </div>
+                <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    minLength="6"
+                    value={password}
+                    onChange={ e => onChange(e) }
+                    required
+                />
+                </div>
+
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </>
 }
 
-export default Login
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+// Make the destructured login from state availbale in the Login component as props
+export default connect(mapStateToProps, { login })(Login);
+
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,49 +1,158 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-
+// import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { register } from '../../redux/actions/auth';
+import toastify from '../layout/Toastify';
 
 
-const Register = ({ dev, isAuthenticated, register }) => {
+const Register = ({ register, isAuthenticated}) => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    });
 
-    useEffect(() => {
-        // Hard-coding the supposed login values for now.
-       const oDev = {
-            name: 'Roland Oben',
-            email: 'myname@example.com',
-            password: 'abc1234'
-        };
+    // Destructure the login properties from formData in order to avoid prefixing formData to the properties
+    const { name, email, password, password2 } = formData;
 
-        register(oDev); // Use the REGISTER_SUCCES action creator to dispatch Register success.       
-    }, []);
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    return (
-        // Get the state slice "auth.user" passed by the mapStateToProps method as props.dev, and read the values.
-        dev && (
-            isAuthenticated && (
-            <div>
-                <h1>Is the current User authenticated? { isAuthenticated ? ': True' : ': False.' }</h1>
-                <ul>
-                    <li>Name: { dev.name }</li>
-                    <li>Name: { dev.email }</li>
-                </ul>
-            </div>
-            )            
-        )
-    )
+    const onSubmit = async e => {
+        e.preventDefault();
+        if (password !== password2) {
+            toastify.error('Passwords do not match!');
+        } else {
+            // Normally, at this stage, the user data should be sent to the backend for database persistence.
+            // Broadcast Register success and send the registered data that comes back from the backend to the store.
+            toastify.success('Registration Successful!');
+            // Send to store
+            register({
+                name,
+                email,
+                password
+            });
+        }
+    }
+
+
+    if (isAuthenticated) {
+        return <Redirect to='/project-dashboard' />
+    }
+
+    return <>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                <input 
+                    type="text" 
+                    placeholder="Name" 
+                    name="name"
+                    value={name} 
+                    onChange={ e => onChange(e) }
+                />
+                </div>
+                <div className="form-group">
+                <input 
+                    type="email"
+                    placeholder="Email Address"
+                    name="email"
+                    value={email}
+                    onChange={ e => onChange(e) }
+                />
+                <small className="form-text">
+                </small>
+                </div>
+                <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={ e => onChange(e) }
+                />
+                </div>
+                <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    name="password2"
+                    value={password2}
+                    onChange={e => onChange(e)}
+                />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </>
 }
 
 Register.propTypes = {
     register: PropTypes.func.isRequired,
-    dev: PropTypes.object.isRequired,
     isAuthenticated: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated,
-    dev: state.auth.user
+    isAuthenticated: state.auth.isAuthenticated
 });
 
 // Map state to the props of this Register component while destructuring at the same time, and these props could be retrieved by passing destructured props in the component parameters input.
-export default connect(mapStateToProps, { register })(Register);
+export default connect(mapStateToProps, { register})(Register);
+
+
+
+
+// import React, { useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+
+// import { register } from '../../redux/actions/auth';
+
+
+// const Register = ({ dev, isAuthenticated, register }) => {
+
+//     useEffect(() => {
+//         // Hard-coding the supposed login values for now.
+//        const oDev = {
+//             name: 'Roland Oben',
+//             email: 'myname@example.com',
+//             password: 'abc1234'
+//         };
+
+//         register(oDev); // Use the REGISTER_SUCCES action creator to dispatch Register success.       
+//     }, []);
+
+//     return (
+//         // Get the state slice "auth.user" passed by the mapStateToProps method as props.dev, and read the values.
+//         dev && (
+//             isAuthenticated && (
+//             <div>
+//                 <h1>Is the current User authenticated? { isAuthenticated ? ': True' : ': False.' }</h1>
+//                 <ul>
+//                     <li>Name: { dev.name }</li>
+//                     <li>Name: { dev.email }</li>
+//                 </ul>
+//             </div>
+//             )            
+//         )
+//     )
+// }
+
+// Register.propTypes = {
+//     register: PropTypes.func.isRequired,
+//     dev: PropTypes.object.isRequired,
+//     isAuthenticated: PropTypes.bool
+// };
+
+// const mapStateToProps = state => ({
+//     isAuthenticated: state.auth.isAuthenticated,
+//     dev: state.auth.user
+// });
+
+// // Map state to the props of this Register component while destructuring at the same time, and these props could be retrieved by passing destructured props in the component parameters input.
+// export default connect(mapStateToProps, { register })(Register);
