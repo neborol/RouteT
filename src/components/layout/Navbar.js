8b6897a1db439144452411c112161a6e90266dcd
Roland Oben <neborol@gmail.com>
2021-01-01 02:50:32 -0500

    Modified the Navbar component to let it show appropriate links when user is logged in and when user is not logged in.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,55 +1,70 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { logout } from '../../redux/actions/auth';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 
 
-const Navbar = () => {
-    const links = (
-      <>
-      <ul>
-        <li>
-            <Link to="/approved">
-                Approved
-            </Link>
-        </li>
-        <li>
-            <Link to="/project-dashboard">
-                <span className='hide-sm'>Dashboard</span>
-            </Link>
-        </li>
-        <li>
-                Logout
-        </li>
-      </ul>
-    
+const Navbar = ({auth: { isAuthenticated }, logout }) => {
+  const publicLinks = (
+    <ul>
+      <li>
+          <Link to="/ideas-pool">
+              All Ideas
+          </Link>
+      </li>
+      <li>
+          <Link to="/project-dashboard">
+              <span className='hide-sm'>Project Dashboard</span>
+          </Link>
+      </li>
+      <li><Link to="/register">Register</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
+  );
 
-   
-        <ul>
-            <li>
-                <Link to="/ideas-pool">
-                    All Ideas
-                </Link>
-            </li>
-            <li>
-                <Link to="/profile">
-                    My Profile
-                </Link>
-            </li>
-          <li><Link to="/register">Register</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
-        </>
-    );
+
+  const privateLinks = (
+    <ul>
+      <li>
+          <Link to="/approved">
+              Approved
+          </Link>
+      </li>
+      <li>
+          <Link to="/profile">
+              My Profile
+          </Link>
+      </li>
+      <li>
+        <Link to="#!" onClick={logout}>
+          Logout
+        </Link>
+      </li>
+    </ul>
+  );
+
    
 
     return (
       <nav className="navbar bg-dark">
-        <h1>
-          <Link to="/"> Devability</Link>
-        </h1>
-        {links}
+        <span>
+          <img className="app-logo" src="assets/images/logo.png" alt="App logo" />
+          <h1><Link to="/"> Devability</Link></h1> 
+        </span>
+        { isAuthenticated ? privateLinks : publicLinks }
       </nav>
     )
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(Navbar); 
