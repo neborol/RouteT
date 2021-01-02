2913dfc4c10013c8d66bd03197ce34d48dbf58ac
Roland Oben <neborol@gmail.com>
2021-01-02 14:10:26 -0500

    Added the MyDashboard component, that would be used to build my personal dashboard.

---
+++ src/components/dashboard/MyDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyDashboard = () => {
+    return (
+        <div>
+            <h1>This is my persoal Dashboard page</h1>
+        </div>
+    )
+}
+
+export default MyDashboard
