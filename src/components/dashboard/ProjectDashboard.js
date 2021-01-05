5b5b13a0d2aa0d9d08e15dcb5d9db4e1729fcb71
Roland Oben <neborol@gmail.com>
2021-01-05 15:41:03 -0500

    Modified the ProjectDashboard to show my direction of thinking.

--- src/components/dashboard/ProjectDashboard.js
+++ src/components/dashboard/ProjectDashboard.js
@@ -6,13 +6,43 @@ import { connect } from 'react-redux';
 const ProjectDashboard = ({ setLoginStatus }) => {
     useEffect(() => {
         const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        currentUser.loggedIn ? setLoginStatus(true) : setLoginStatus(false);
+        currentUser.isAuthenticated ? setLoginStatus(true) : setLoginStatus(false);
     }, []);
 
     return (
-        <div>
-            <h1>This is the Project's Dashboard page</h1>
-        </div>
+        <>
+            <h1 className='large text-primary'>Team Dashboard</h1>
+            <p className="lead">
+                <span style={{fontSize: '26px'}}>Welcome Team Members</span>
+            </p>
+
+            { 
+                <>
+                    <h3>Here, you can browse different sections to see how we are doing as a team.</h3>
+                    <p>The individual boxes below show the different sections of all our projects.</p>
+                    <p>Click on a box to see the details.</p>
+                     <div>-----  <strong> Graphical Representation of different sections come here. </strong>   -----</div>   
+                
+                    <br/><br/>
+                    <div className="dashboard-display">
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                        <div className="dashboard-items"></div>
+                    </div>
+                    
+                </>
+            } 
+        </>
     )
 }
 
